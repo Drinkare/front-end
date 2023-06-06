6b819277a44ee3cd4eaef710eb4cbe8029361b04
dhsin98 <shinjay417@gmail.com>
2023-06-07 01:05:17 +0900

    url

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -15,7 +15,8 @@ const Login = () => {
   // 카카오 로그인 함수를 실행시키면 아래에 설정해 놓은 KAKAO_AUTH_URL 주소로 이동한다.
   // 이동 된 창에서 kakao 계정 로그인을 시도할 수 있으며 로그인 버튼 클릭 시 Redirect URI로 이동하면서 빈 화면과 함게 인가코드가 발급된다.(인가코드는 파라미터 값에 들어가 있다!)
   const REST_API_KEY = "532d7168dd0821f3756ea1293ba8dea4";
-  const REDIRECT_URI = "http://localhost:3000/login";
+  // const REDIRECT_URI = "http://localhost:3000/login";
+  const REDIRECT_URI = "https://drinkare.netlify.app/";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   // const KAKAO_AUTH_URL = `https://kauth.kakao.com/login/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
